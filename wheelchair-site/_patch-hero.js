a390ae81b9e523c6155cf0a89bb9c0acd1ef7b1a
xiaoyang11-ycl <286000398@qq.com>
2026-05-21 01:21:27 +0800

    Delete wheelchair-site directory

--- wheelchair-site/_patch-hero.js
+++
@@ -1,8 +0,0 @@
-﻿const fs = require("fs");
-const path = require("path");
-const root = __dirname;
-let html = fs.readFileSync(path.join(root, "index.html"), "utf8");
-html = html.replace(/<h1 class="home-hero__title">[\s\S]*?<\/h1>/, '<h1 class="home-hero__title">舒适出行，从定制开始</h1>');
-html = html.replace("匹配配件", "配备配件");
-fs.writeFileSync(path.join(root, "index.html"), html);
-console.log("index ok");
--- wheelchair-site/打开预览.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-cd /d "%~dp0"
-start /min "" node server-preview.js
-timeout /t 1 /nobreak >nul
-start "" "http://127.0.0.1:8765/"
